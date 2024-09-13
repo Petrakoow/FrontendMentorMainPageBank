56f63140c45717dd75f9f68677e5e0f05aea5190
Petrakowww <Petrakov.Egor.G@yandex.ru>
2024-09-13 04:52:54 +0300

    fx err

--- app/js/script.js
+++ app/js/script.js
@@ -2,11 +2,10 @@ function showHamburger(){
     const btnHamburger = document.querySelector('#btnHamburger');
 
     btnHamburger.addEventListener('click', e => {
-        if (e.target.classList.contains('open')){
-            e.target.classList.remove('open');
-        }
-        else{
-            e.target.classList.add('open');
+        if (btnHamburger.classList.contains('open')) {
+            btnHamburger.classList.remove('open');
+        } else {
+            btnHamburger.classList.add('open');
         }
     });
 }
