be8c15d2309cfd2828b05f0ab63cf73891b1d3ce
Petrakowww <Petrakov.Egor.G@yandex.ru>
2024-09-13 16:59:29 +0300

    edit keyframes

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,15 @@
 function showHamburger(){
     const btnHamburger = document.querySelector('#btnHamburger');
+    const header = document.querySelector('.header');
+    const fade = document.querySelector('.has-fade');
 
-    btnHamburger.addEventListener('click', e => {
-        if (btnHamburger.classList.contains('open')) {
-            btnHamburger.classList.remove('open');
-        } else {
-            btnHamburger.classList.add('open');
-        }
+    btnHamburger.addEventListener('click', () => {
+        header.classList.toggle('open');
+        fade.classList.toggle('fade-in', header.classList.contains('open'));
+        fade.classList.toggle('fade-out', !header.classList.contains('open'));
     });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     showHamburger();
-})
+});
